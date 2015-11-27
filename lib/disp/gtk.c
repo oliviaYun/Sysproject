01acbf4151b5d8cf06a22c855cc712691d604bda
jungyun Yun <ab8795@naver.com>
2015-11-26 22:44:09 -0800

    gtk

--- lib/disp/gtk.c
+++ lib/disp/gtk.c
@@ -4,16 +4,18 @@ int
 main (int   argc,
 char *argv[])
 {
+ GtkWidget *plot;
   GtkWidget *window;
 
   gtk_init (&argc, &argv);
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
+ plot = gtk_image_new_from_file("plot.png");
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
   gtk_widget_show (window);
-
+ gtk_widget_show(iam)
   gtk_main ();
 
   return 0;
