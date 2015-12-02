eef4afd1648957cd5e7898df363d4be1accddd17
Lee Minho <minhoo3@naver.com>
2015-12-03 00:45:07 +0900

    close modifi

---
+++ inc/close.h
@@ -0,0 +1,8 @@
+#ifndef CLOSE_H_
+#define CLOSE_H_
+
+void close(double **num,int *retncnt, double **den, int *retdcnt, double **hnum, int *hncnt, double **rethden, int *rethdcnt);
+
+
+#endif /* CLOSE_H_ */
+
--- lib/inter/close.c
+++ lib/inter/close.c
@@ -3,61 +3,45 @@
 #include <tgmath.h>
 #include <string.h>
 #include "open.h"
-
-void close(void)
+#include "close.h"
+void close(double **num, int *retncnt, double **den, int *retdcnt, double **hnum, int *hncnt, double **rethden, int *rethdcnt)
 {
 	
-	double complex z1 = I;
-	char buf[50];
+	char buf[50]={0,};
+	char dbuf[50]={0,};
 	int i,ncnt,dcnt,hncnt,hdcnt;
-	double *num, *den, *hnum, *hden;
-	printf("Insert Plant's Numerator\n");
+
+	fflush(stdin);
+	printf("Insert Plant Numerator\n");
 	fflush(stdin);
 	fgets(buf,sizeof(buf),stdin);
 	ncnt = count_num(buf);
-	num = (double *)malloc(sizeof(double)*ncnt);
-	atoiarray(buf,num,ncnt);
-	printf("Insert Plant's Denomenator\n");
+	*num = (double *)malloc(sizeof(double)*ncnt);
+	atoiarray(buf,*num,ncnt);
+	printf("Insert Plant Denomenator\n");
 	fflush(stdin);
 	fgets(buf,sizeof(buf),stdin);
 	dcnt = count_num(buf);
-	den = (double *)malloc(sizeof(double)*dcnt);
-	atoiarray(buf,den,dcnt);
-
-	printf("Insert Controller's Numerator\n");
+	*den = (double *)malloc(sizeof(double)*dcnt);
+	atoiarray(buf,*den,dcnt);
+	printf("Insert Controller Numerator\n");
 	fflush(stdin);
 	fgets(buf,sizeof(buf),stdin);
 	hncnt = count_num(buf);
-	hnum = (double *)malloc(sizeof(double)*hncnt);
-	atoiarray(buf,hnum,ncnt);
-	printf("Insert Controller's Denomenator\n");
+	*hnum = (double *)malloc(sizeof(double)*hncnt);
+	atoiarray(buf,*hnum,hncnt);
+	printf("Insert Controller Denomenator\n");
 	fflush(stdin);
 	fgets(buf,sizeof(buf),stdin);
 	hdcnt = count_num(buf);
-	hden = (double *)malloc(sizeof(double)*hdcnt);
-	atoiarray(buf,hden,dcnt);
-	for(i = 0; i < ncnt; i++)
-	{
-		if(i == (ncnt-1))
-			printf("%f \n");
-		else
-			printf("%fs^%d +",num[i],(ncnt-i-1));
-
-	}
-	printf("----------------------------------------------\n");
-
-	for(i = 0; i < dcnt; i++)
-	{
-		if(i==(dcnt-1))
-			printf("%f \n");
-		else
-			printf("%fs^%d +",den[i],(dcnt-i-1));
-	}
-	return 0;
+	*hden = (double *)malloc(sizeof(double)*hdcnt);
+	atoiarray(buf,*hden,hdcnt);
+//	printf("return\n");
+	*retncnt = ncnt;
+	*retdcnt = dcnt;
+	*rethncnt = hncnt;
+	*rethdcnt = hdcnt;
+//	printf("%d %d\n",*retncnt,*retdcnt);
+	return;
 }
 
-int main(void)
-{
-	close();
-	return 0;
-}
