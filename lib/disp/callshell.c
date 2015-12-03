54e7bab3796b480d9fc2c38dfb4900dc53a94c19
jungyun Yun <ab8795@naver.com>
2015-12-02 20:52:52 -0800

    callshell

--- lib/disp/callshell.c
+++ lib/disp/callshell.c
@@ -3,26 +3,38 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int bodeplot(char *command)
+int bodeplot(char *b_command)
 {
-
+	if(fork()==0)
+	{
+		execl("/bin/bash","bash","-c",b_command,NULL);
+		exit(123);
+	}
+	else
+	{
+		wait();
+	}
 
 
 }
-int nyquist(char *command)
+int nyquist(char *c_command)
 {
-
-
-
+	 if(fork()==0)
+        {
+                execl("/bin/bash","bash","-c",c_command,NULL);
+                exit(123);
+        }
+        else
+        {
+                wait();
+        }
 }
 
 int main (void)
 {
 	int b_command;
 	int n_command;
-	b_command = bodeplot("");
-	n_command = nyquist("");
-	
+	b_command = bodeplot("./bode.sh");
+	//n_command = nyquist("./nyquist.sh");
 	return 0;
-
 }
