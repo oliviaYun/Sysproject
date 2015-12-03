f80fc8b124b2a7b5acfff690027e48d8b76c6a6a
jungyun Yun <ab8795@naver.com>
2015-12-02 20:31:01 -0800

    cfile

---
+++ lib/disp/callshell.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int bodeplot(char *command)
+{
+
+
+
+}
+int nyquist(char *command)
+{
+
+
+
+}
+
+int main (void)
+{
+	int b_command;
+	int n_command;
+	b_command = bodeplot("");
+	n_command = nyquist("");
+	
+	return 0;
+
+}
