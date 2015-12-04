258adaa1ae6168217ed2230e9d4a9431affa36ae
Lee Minho <minhoo3@naver.com>
2015-12-04 13:53:20 +0900

    nyquist calc update

---
+++ inc/nyquist.h
@@ -0,0 +1,7 @@
+#ifndef NYQUIST_H_
+#define NYQUIST_H_
+
+void nyquist(int stauts, int mode);
+
+#endif /* NYQUIST_H_ */
+
--- lib/calc/nyquist.c
+++ lib/calc/nyquist.c
@@ -1,42 +1,60 @@
 //compile like this! gcc -o test testfile.c -I../../inc/ -L. -lopen -lm
 #include <stdio.h>
+#include <stdlib.h>
 #include "myopen.h"
+#include "myclose.h"
+#include "bode.h"
 #include <complex.h>
 #include <tgmath.h>
-double complex devide (double complex x, double complex y){
-
-	double complex z;
-	z = (creal(x)*creal(y)+cimag(x)*cimag(y))/(creal(y)*creal(y)+cimag(y)*cimag(y));
-	z+=((cimag(x)*creal(y)-creal(x)*cimag(y))/(creal(y)*creal(y)+cimag(y)*cimag(y)))*I;
-	return z;
-}
-
-void nyquist(void)
+void nyquist(int status, int mode)
 {
 	FILE *fp;
-	double *num, *den, w;
-	int retncnt, retdcnt, i, j;
-	double complex znum, zden, z;
+	double *num, *den, *hnum, *hden, w;
+	int retncnt, retdcnt,rethncnt,rethdcnt, i, j;
+	double complex znum, zden, z,G,H,zhnum,zhden;
 	double real;
 	retncnt = 0; retdcnt = 0;
-	open(&num,&retncnt,&den,&retdcnt);
-	printf("ncnt : %d, dnct : %d\n",retncnt,retdcnt);
+	if(status == 0)
+		myopen(&num,&retncnt,&den,&retdcnt);
+	else
+		myclose(&num,&retncnt,&den,&retdcnt,&hnum,&rethncnt,&hden,&rethdcnt);
+	printf("G is \n");
+//	printf("ncnt : %d, dnct : %d\n",retncnt,retdcnt);
 	for(i = 0; i < retncnt; i++)
 	{
-		printf("%.2f ^ %d + ",num[i],retncnt-i);
+		printf("%.2f ^ %d + ",num[i],retncnt-i-1);
 	}
 	printf("\n-----------------\n");
 	for(i = 0; i < retdcnt; i++)
 	{
-		printf("%.2f ^ %d + ",den[i],retdcnt-i);
+		printf("%.2f ^ %d + ",den[i],retdcnt-i-1);
 	}
 	printf("\n");
-	fp = fopen("../disp/result2.txt","w+");
+	
+	if(status!=0)
+	{
+		printf("H is \n");
+		for(i = 0; i < rethncnt; i++)
+		{
+			printf("%.2f ^ %d + ",hnum[i],rethncnt-i-1);
+		}
+		printf("\n-----------------\n");
+		for(i = 0; i < rethdcnt; i++)
+		{
+			printf("%.2f ^ %d + ",hden[i],rethdcnt-i-1);
+		}
+		printf("\n");
+	
+	}
+	fp = fopen("result2.dat","w+");
 	for(w = 0.1; w <10; w+=0.001)
 	{
 //		w = 1;
 		znum = 0;
 		zden = 0;
+		zhnum = 0;
+		zhden = 0;
+
 		for(i = 0; i < retncnt; i++)
 		{
 			if((retncnt-i-1)==0)
@@ -51,8 +69,38 @@ void nyquist(void)
 			else
 				zden+=den[i]*pow(w*I,retdcnt-i-1);
 		}		
-		
-		fprintf(fp,"%f %f\n",creal(devide(znum,zden)),cimag(devide(znum,zden)));
+		G = devide(znum,zden);
+		if(status != 0)
+		{	
+			for(i = 0; i < rethncnt; i++)
+			{
+				if((rethncnt-i-1)==0)
+					zhnum+=hnum[i];
+				else
+					zhnum+=hnum[i]*pow(w*I,rethncnt-i-1);
+			}
+			for(i = 0; i < rethdcnt; i++)
+			{
+				if((rethdcnt-i-1)==0)
+					zhden+=hden[i];
+				else
+					zhden+=hden[i]*pow(w*I,rethdcnt-i-1);
+			}
+			H = devide(zhnum,zhden);		
+		}
+		double complex result;
+		if(status == 0 && mode == 0)
+			result = G;
+		else if(status == 0 && mode == 1)
+			result = devide(G,(1+G));
+		else if(status == 1 && mode == 0)
+			result = G*H;
+		else if(status == 1 && mode == 1)
+			result = devide(G*H, 1+G*H);
+		else{
+			printf("wrong parameters\n");
+			exit(1);
+		}	
+		fprintf(fp,"%lf %lf\n",w,creal(result),cimag(result));
 	}
-
 }
